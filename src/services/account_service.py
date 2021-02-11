a6994d7f66ec09011347820f1e80eed52a9702a9
afkpuezo <andrewfcurry@gmail.com>
2021-02-11 12:39:09 -0500

    wrote and tested account_service's deposit method. also for some reason pip is here

--- src/services/account_service.py
+++ src/services/account_service.py
@@ -79,3 +79,41 @@ class AccountService():
                     return target_account
         except DAOException as e:
             raise ServiceException("DAOException: " + e.message)
+    
+    def deposit(self, current_username: str, account_id: int, amount: int) -> Account:
+        """
+        A customer makes a deposit into their own account.
+        Fails and raises a ServiceException if:
+            - the current user does not exist
+            - the user is not a customer
+            - the account does not exist
+            - the user does not own the account
+            - the account is not open/approved
+            - the funds amount is not positive
+            - there is a DAO exception
+        """
+        try:
+            current_user: User = self.user_dao.find(current_username)
+            if not current_user:
+                raise ServiceException("User '" + current_username + "' not found.")
+            elif current_user.level != UserLevel.CUSTOMER:
+                raise ServiceException("Only CUSTOMERS can deposit funds.")
+            else:  # if user is valid
+                target_account: Account = self.account_dao.find(account_id)
+                if not target_account:
+                    raise ServiceException("Account #" + str(account_id) + " not found.")
+                elif target_account.owner_username != current_username:
+                    raise ServiceException(
+                            "You do not have permission to access Account #" 
+                            + str(account_id))
+                elif not target_account.is_approved:
+                    raise ServiceException(
+                            "Account #" + str(account_id) + " has not yet been approved.")
+                elif amount <= 0:
+                    raise ServiceException("You must deposit a positive amount of funds.")
+                else:  # valid user and account and amount
+                    target_account.funds += amount
+                    return self.account_dao.write(target_account)
+        except DAOException as e:
+            raise ServiceException("DAOException: " + e.message)
+        
